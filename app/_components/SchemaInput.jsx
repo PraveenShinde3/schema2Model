95b510c5b6cb66202866c102546bd559180db94b
Praveen Shinde <shindepraveen002@gmail.com>
2024-10-20 15:12:27 +0530

    Added Db select in schemaInput

--- app/_components/SchemaInput.jsx
+++ app/_components/SchemaInput.jsx
@@ -9,6 +9,7 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
 
 const SchemaInput = ({ generateModels }) => {
   const selectlanguages = [
@@ -24,18 +25,35 @@ const SchemaInput = ({ generateModels }) => {
     "Swift",
     "Rust",
   ];
+  const most_used_databases = [
+    "MySQL",
+    "PostgreSQL",
+    "Oracle Database",
+    "Microsoft SQL Server",
+    "MongoDB",
+    "Cassandra",
+    "Redis",
+    "SQLite",
+    "MariaDB",
+    "Firebase",
+  ];
 
   const [language, setLanguage] = useState("JavaScript");
+  const [db, setDb] = useState("MongoDB");
   const [schema, setSchema] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  const onSubmit = () => {
+  const onSubmit = async () => {
     try {
+      setIsLoading(true);
       if (schema) {
-        generateModels(schema, language);
+        await generateModels(schema, language);
       } else {
         alert("Please enter a SQL schema");
       }
+      setIsLoading(false);
     } catch (e) {
+      setIsLoading(false);
       console.error("Error generating models:", e);
     }
   };
@@ -51,30 +69,51 @@ const SchemaInput = ({ generateModels }) => {
           onChange={(e) => setSchema(e.target.value)}
         />
         <div className="flex justify-between w-full items-center">
-          <Select
-            value={language}
-            onValueChange={(value) => setLanguage(value)}
-          >
-            <SelectTrigger className="w-[100px]">
-              <SelectValue>
-                {language
-                  ? language.charAt(0).toUpperCase() + language.slice(1)
-                  : "Javascript"}
-              </SelectValue>
-            </SelectTrigger>
-            <SelectContent>
-              {selectlanguages.map((language) => (
-                <SelectItem
-                  className="text-[0.7rem]"
-                  value={language}
-                  key={language}
-                >
-                  {language}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
+          <div className="flex gap-2">
+            <Select
+              value={language}
+              onValueChange={(value) => setLanguage(value)}
+            >
+              <SelectTrigger className="w-[100px]">
+                <SelectValue>
+                  {language
+                    ? language.charAt(0).toUpperCase() + language.slice(1)
+                    : "Javascript"}
+                </SelectValue>
+              </SelectTrigger>
+              <SelectContent>
+                {selectlanguages.map((language) => (
+                  <SelectItem
+                    className="text-[0.7rem]"
+                    value={language}
+                    key={language}
+                  >
+                    {language}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+            <Select value={language} onValueChange={(value) => setDb(value)}>
+              <SelectTrigger className="w-[120px]">
+                <SelectValue>
+                  {db ? db.charAt(0).toUpperCase() + db.slice(1) : "MongoDb"}
+                </SelectValue>
+              </SelectTrigger>
+              <SelectContent>
+                {most_used_databases.map((language) => (
+                  <SelectItem
+                    className="text-[0.7rem]"
+                    value={language}
+                    key={language}
+                  >
+                    {language}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
           <Button size="custom" onClick={onSubmit}>
+            {isLoading && <Loader2 className=" p-[1px] animate-spin" />}
             Generate Models
           </Button>
         </div>
